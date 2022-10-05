1338f684397b9f1bf09921d6576653739b980c0c
RateThisMust <work@alex-makarov.ru>
2022-10-05 13:13:25 +0300

    f

---
+++ config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'displayErrorDetails' => true,
+    'secret' => 'c4e250aed1813a0b4bbe5a01532649',
+    'root_dir' => 'D:\OSPanel\domains\wbtools\server',
+    'domain' => '//app.rate-this.ru',
+    'dadata_api_key' => '57eb82c6a4b76b07c18a8822dcc60d126073a2c9',
+    'jwt_salt' => '779560',
+];
---
+++ config/connection.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    // prod
+    // 'rate' => [
+    //     'driver' => 'mysql',
+    //     'host' => 'j45316134.myjino.ru',
+    //     'port' => '3306',
+    //     'db' => 'j45316134',
+    //     'user' => 'j45316134',
+    //     'password' => 'cf}j}R75tRzM'
+    // ]
+
+    // dev
+    'rate' => [
+        'driver' => 'mysql',
+        'host' => 'j45316134.myjino.ru',
+        'port' => '3306',
+        'db' => 'j45316134',
+        'user' => 'j45316134',
+        'password' => 'cf}j}R75tRzM'
+    ]
+];
+
+
+
